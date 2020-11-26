01dc1769746f9d31fdd0b6ecb8fcf08a83e5e60f
weslynSouza <wesley.i476@gmail.com>
2020-11-26 19:23:36 -0300

    Primeiros projetos com VS

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+            int idade = 32;
+
+            Console.WriteLine("Idade: " + idade);
+
+            double salario = 1200.70;
+
+            Console.WriteLine("O salario do funcionario é :" + salario + "!");
+
+            int salarioInteiro;
+
+            salarioInteiro = (int)salario;
+
+            Console.WriteLine(salarioInteiro);
+
+            Console.WriteLine("Execução acabou. Tecle enter para sair. . .");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 3-CaracteresETextos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+            // Character
+            char primeiraLetra = 'a';
+
+            Console.WriteLine(primeiraLetra);
+
+            // Texto
+            string titulo = "Alura Cursos de tecnologia";
+
+            Console.WriteLine(titulo);
+            
+            Console.ReadLine();
+        }
+    }
+}
